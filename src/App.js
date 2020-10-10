949efb3d313322bc6abbdf6d846248a57bd3e5a5
Ryukyo <florian.fejer@hotmail.de>
2020-10-10 19:02:33 +0200

    Basic header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './assets/logo.png';
 import './App.css';
+import Head from "./components/head.jsx"
 
 function App() {
   return (
     <div className="App">
+      <Head numberOfMails="5"></Head>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
