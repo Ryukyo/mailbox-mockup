3aea397eaff8df0081ff4f18d99301865800e1a3
Ryukyo <florian.fejer@hotmail.de>
2020-10-11 12:58:18 +0200

    Datepicker and svg icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './assets/logo.png';
 import './App.css';
-import Head from "./components/head.jsx"
+import Head from "./components/Head"
 
 function App() {
+
+  const startSearchByDate = (startDate, endDate) => {
+    console.log("searching")
+  }
+
   return (
     <div className="App">
-      <Head numberOfMails="5"></Head>
+      <Head numberOfMails="5" search={startSearchByDate}></Head>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
---
+++ src/components/Head.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import '../style/head.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import calendar from "../assets/icon_calender.svg"
+import search from "../assets/icon_search.svg"
+
+export default function Head ({numberOfMails}) {
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(null);
+    const [inputValue, setInputValue] = useState(startDate)
+  
+    const onChange = dates => {
+      const [start, end] = dates;
+      setStartDate(start);
+      setEndDate(end);
+      console.log(startDate, end)
+      setInputValue(startDate)
+      console.log(inputValue)
+    };
+
+    const startSearchByDate = (startDate, endDate) => {
+      console.log("searching")
+    }
+
+    return (
+        <div className="head">
+           <div className="searchByDate">
+             <img src={calendar} className="calendar" alt="calendar" width="25"/>
+               <DatePicker
+                id="date"
+                selected={inputValue}
+                onChange={onChange}
+                startDate={startDate}
+                endDate={endDate}
+                selectsRange
+                dateFormat={["yyyy/M/d", "yyyy/M/d"]}
+              />
+              <img src={search} className="search" alt="search" width="30" onClick={() => {startSearchByDate(startDate, endDate)}}/> 
+           </div> 
+
+           <div className="resultNumber">
+            Results: {numberOfMails} mail(s)
+           </div>
+           <hr className="headerLine"/>
+        </div>
+    )
+}
