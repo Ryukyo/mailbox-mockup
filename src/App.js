6d04d041a486b6e13f7ecc28a9f1365648dca618
Ryukyo <florian.fejer@hotmail.de>
2020-10-14 22:13:12 +0200

    Correctly filter No of emails and emails

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 import Head from "./components/Head"
 import Mailbox from "./components/Mailbox"
 import Logo from "./components/Logo"
+import moment from 'moment'
 
 const emailsFromJSON = require('./assets/emails.json')
 
 function App() {
   const [numberOfMails, setNumberOfMails] = useState(0)
+  const [emailsToDisplay, setEmailsToDisplay] = useState([])
 
   const startSearchByDate = (startDate, endDate) => {
-    console.log(emailsFromJSON)
-    setNumberOfMails(emailsFromJSON.length)
-
+    // Filter emails from JSON sample file (usually get the from endpoint) by start and end date (including)
+    let mailsWithinDateRange = emailsFromJSON.filter((mail) => {
+      console.log(mail.date)
+      return moment(mail.date).isBetween(startDate, endDate, "day", true)
+    })
+     
+    // Set number of mails to length of filtered array and pass the filtered array to mailbox component
+    setEmailsToDisplay(mailsWithinDateRange)
+    setNumberOfMails(mailsWithinDateRange.length)
   }
 
   return (
     <div className="App">
       <Head numberOfMails={numberOfMails} search={startSearchByDate}></Head>
-      {numberOfMails <= 0? <Logo/> : <Mailbox emails={emailsFromJSON}/>}
+      {numberOfMails <= 0? <Logo/> : <Mailbox emails={emailsToDisplay}/>}
     </div>
   );
 }
