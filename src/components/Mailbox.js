eba9c1fb38076ba3def9d5732e2ee3d7a3fc9c54
Ryukyo <florian.fejer@hotmail.de>
2020-10-14 20:45:54 +0200

    Toggle selected email display

--- src/components/Mailbox.js
+++ src/components/Mailbox.js
@@ -4,27 +4,34 @@ import Email from './Email'
 import EmptySelection from './EmptySelection'
 
 export default function Mailbox (props) {
-    const [emailId, setEmailId] = useState(null)
-    let selectedEmail = <EmptySelection/>
+    const [emailId, setEmailId] = useState([null])
+    const [selectedEmail, setSelectedEmail] = useState("")
 
     useEffect(() => {
-        if(emailId) {
+        if(emailId[0] !== null) {
             let mailToDisplay = props.emails.filter((mail) => {
-                return mail.id === emailId
-            })[0];
-            selectedEmail = <Email 
+              return mail.id === emailId[0]
+            });
+            console.log("mail to display", mailToDisplay)
+            setSelectedEmail(<Email 
                 id={mailToDisplay.id}
                 from={mailToDisplay.from}
                 to={mailToDisplay.to}
                 subject={mailToDisplay.subject}
                 date={mailToDisplay.date}
                 body={mailToDisplay.body}
-            />
+            />)
         }
-    }, [])
+    }, emailId)
 
       const handleSelectEmail = (id) => {
-        setEmailId(id);
+        if (emailId[0] === null) {
+          setEmailId([id]);
+          console.log("set id to", emailId)
+        } else {
+          setEmailId([null])
+          console.log("set id to", emailId)
+        }
       };
 
     return (
@@ -32,7 +39,7 @@ export default function Mailbox (props) {
         <EmailList emails={props.emails}
                    onSelectEmail={handleSelectEmail} />
         <div className="email-viewer">
-          {selectedEmail}
+          {emailId[0] !== null? selectedEmail : <EmptySelection/>}
         </div>
       </div>
     )
